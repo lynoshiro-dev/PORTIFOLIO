251fd8e1b7e41e6bbd52cf4fafa44943b7bd4e46
Lyn Osiro <evyoshiroc.c@gmail.com>
2025-04-13 16:03:37 -0300

    Update java.js

--- java.js
+++ java.js
@@ -1,5 +1,1 @@
-document.getElementById("contact-fomr").addEventListener("submit",function(e){
-    e.preventDefault();
-    document.getElementById("message").textContent= "Mensagem enviada com sucesso! Obrigada pelo contato.";
-    this.reset();
-})
+
